f2df5f84b46bdff0474ab0ee4eb93a4eaae44b1d
caressapy <caressapy@gmail.com>
2025-03-21 00:12:51 +0700

    Validating request and selectively responding

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,32 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    println!("Server listening on 127.0.0.1:7878");
+fn main() -> () {
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("Connection established!");
-                handle_connection(stream);
-            }
-            Err(e) => eprintln!("Connection failed: {}", e),
-        }
+        let stream: TcpStream = stream.unwrap();
+
+        handle_connection(stream);
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.expect("Failed to read line"))
-        .take_while(|line| !line.is_empty())
-        .collect();
+fn handle_connection(mut stream: TcpStream) -> () {
+    let buf_reader: BufReader<&mut TcpStream> = BufReader::new(&mut stream);
+    let request_line: String = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {:#?}", http_request);
+    let (status_line, filename): (&str, &str) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "oops.html")
+    };
 
-    let status_line = "HTTP/1.1 200 OK";
-    
-    // Pastikan file `hello.html` ada di direktori yang sama dengan `main.rs`
-    let contents = fs::read_to_string("hello.html").unwrap_or_else(|_| "File not found".to_string());
-    let length = contents.len();
- 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
- 
+    let contents: String = fs::read_to_string(filename).unwrap();
+    let length: usize = contents.len();
+
+    let response: String = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
+
     stream.write_all(response.as_bytes()).unwrap();
 }
