c617e160053b8247513daee764b89cdbd195524d
caressapy <caressapy@gmail.com>
2025-03-20 23:47:26 +0700

    Handle-connection, check response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    let x = 42;
-    let y = 24;
-    println!("Hello, world of 2025!");
-    println!("x + y = {}", x + y);
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("Server listening on 127.0.0.1:7878");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_) => println!("Connection established!"),
+            Err(e) => eprintln!("Connection failed: {}", e),
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.expect("Failed to read line"))
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
